11892a4f3663988d65b4c475d3dd2caaf636d253
DanvdEnde <Danvdende@gmail.com>
2020-01-22 13:57:14 +0100

    Formatting & induced api connection delays

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,23 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Container } from 'semantic-ui-react'
-import axios from 'axios'
-import { IActivity } from '../models/activity'
-import NavBar from '../../features/nav/NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
+import React, { useState, useEffect, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   const handleSelectedActivity = (id: string) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false)
+    setEditMode(false);
   };
 
   const handleOpenCreateForm = () => {
@@ -22,43 +26,58 @@ const App = () => {
   };
 
   const handleCreateActivity = (activity: IActivity) => {
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    })
+    setSubmitting(true);
+    agent.Activities.create(activity)
+      .then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
   const handleEditActivity = (activity: IActivity) => {
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    })
+    setSubmitting(true);
+    agent.Activities.update(activity)
+      .then(() => {
+        setActivities([
+          ...activities.filter(a => a.id !== activity.id),
+          activity
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
   const handleDeleteActivity = (id: string) => {
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)])
-    })
-  }
+    setSubmitting(true);
+    agent.Activities.delete(id)
+      .then(() => {
+        setActivities([...activities.filter(a => a.id !== id)]);
+      })
+      .then(() => setSubmitting(false));
+  };
 
   useEffect(() => {
     agent.Activities.list()
       .then(response => {
         let activities: IActivity[] = [];
         response.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
+          activity.date = activity.date.split(".")[0];
           activities.push(activity);
-        })
+        });
         setActivities(activities);
-      });
+      })
+      .then(() => setLoading(false));
   }, []);
+
+  if (loading) return <LoadingComponent content="loading activities......" />;
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
-      <Container style={{ marginTop: '7em' }}>
+      <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectedActivity}
@@ -69,10 +88,11 @@ const App = () => {
           createActivity={handleCreateActivity}
           editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </Fragment>
   );
-}
+};
 
 export default App;
