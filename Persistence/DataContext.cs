575fa2d5a3b9e8ad33eb7416e302a04f2cdb0a9e
DanvdEnde <Danvdende@gmail.com>
2020-02-04 17:11:34 +0100

    Setup IdentityUser

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
@@ -12,13 +13,15 @@ namespace Persistence
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder) 
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             builder.Entity<Value>()
                 .HasData(
-                    new Value {Id = 1, Name = "One"},
-                    new Value {Id = 2, Name = "Two"},
-                    new Value {Id = 3, Name = "Three"}
+                    new Value { Id = 1, Name = "One" },
+                    new Value { Id = 2, Name = "Two" },
+                    new Value { Id = 3, Name = "Three" }
                 );
         }
     }
